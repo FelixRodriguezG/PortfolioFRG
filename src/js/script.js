3cefdf4d8ba6817ae6c89aa552b925e1d58832a2
FelixRG <correofrg@gmail.com>
2024-01-11 00:22:04 +0100

    footer y about

--- src/js/script.js
+++ src/js/script.js
@@ -1,13 +1,14 @@
-// header scrolldown //
+////////////////////////////////// header scrolldown //////////////////
 
 window.addEventListener("scroll", function(){
     var header =  document.querySelector("header");
     header.classList.toggle("scroll_down",this.window.scrollY>0)
 })
 
-//Animación de texto//
 
-var texto = "Felix Rodriguez Porfolio #2024";
+/////////////////////////////////Animación de texto////////////////////
+
+var texto = "Portfolio #2024 <FRG> ";
   var velocidad = 50; // velocidad de escritura en milisegundos
  
   function escribirTexto(texto, elemento) {
@@ -24,16 +25,18 @@ var texto = "Felix Rodriguez Porfolio #2024";
  
   var textoAnimado = document.getElementById("texto-animado");
   textoAnimado.style.fontSize = "1.5vw"; // ajustar el tamaño del texto al ancho de la ventana del navegador
-  textoAnimado.style.lineHeight = "1.5"; // reducir el interlineado
+  textoAnimado.style.lineHeight = "1.3vw"; // reducir el interlineado
   textoAnimado.style.color = ""; // cambiar el color de la letra a naranja
   escribirTexto(texto, textoAnimado);
    
-  // Actualizar el tamaño del texto al cambiar el tamaño de la ventana del navegador
+  // Actualiza el tamaño del texto al cambiar el tamaño de la ventana del navegador
   window.addEventListener('resize', function() {
-    textoAnimado.style.fontSize = "1vw";
+    textoAnimado.style.fontSize = "1,2vw";
   });
 
-//Animacion saludo//
+
+
+//////////////////////////////////Animacion saludo////////////////////////
 
 const spans = document.querySelectorAll('.word span');
 
@@ -50,3 +53,19 @@ spans.forEach((span, idx) => {
 		span.classList.add('active');
 	}, 750 * (idx+1))
 });
+
+/////////////////////////////Animacion propjectos////////////////////////
+
+const panels = document.querySelectorAll('.panel');
+
+    panels.forEach(panel => panel.addEventListener('click', toggleOpen));
+    panels.forEach(panel => panel.addEventListener('transitionend', toggleActive));
+
+    function toggleOpen() {
+      this.classList.toggle('open');
+    }
+    function toggleActive(e) {
+      if (e.propertyName.includes('flex')) {
+        this.classList.toggle('open-active');
+      }
+    }
